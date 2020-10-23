34556cd1b0fccb6327deffc35d2330a15ada123e
Jiselle Liu <jiselle03@gmail.com>
2020-10-23 14:22:16 -0700

    Remove unnecessary files and reorganize client directories

---
+++ client/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <header>
+      </header>
+
+      <Switch>
+
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default App;
