f089268087a902b3c9377b7379c3aa7727092219
Jiselle Liu <jiselle03@gmail.com>
2020-10-24 14:13:07 -0700

    Add routes for user and session

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do 
+      resources :users, only: [:create] do
+        get :current, on: :collection
+      end
+      resource :session, only: [:create, :destroy]
+    end
+  end
 end
