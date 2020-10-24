dd60ae545ebd6c22a526ea4b18936958c687c93e
Jiselle Liu <jiselle03@gmail.com>
2020-10-24 12:43:11 -0700

    Add methods for finding and authenticating user

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+    private
+
+    def current_user
+        if session[:user_id].present?
+            @current_user ||= User.find_by(id: session[:user_id])
+        end
+    end
+
+    def authenticate_user!
+        unless current_user.present?
+            render( 
+                json: { 
+                    status: 401 
+                }, 
+                status: 401 # Not Authorized
+            )
+        end
+    end
 end
