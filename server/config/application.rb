c49380964863579355a504f4897d8f3d1898ddfa
Jiselle Liu <jiselle03@gmail.com>
2020-10-24 22:53:15 -0700

    Change api_only config to true and add session and cookies middleware manually

--- server/config/application.rb
+++ server/config/application.rb
@@ -32,7 +32,11 @@ module Szyano
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = false
+    config.api_only = true
+    
+    # Manually add session and cookies
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_cookie_name', expire_after: 30.day
 
     # Never generate helper and asset files when controllers are created by default
     config.generators do |g|
