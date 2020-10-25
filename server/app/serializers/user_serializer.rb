d663475bb15b1770d9b8a87972f2882d16801559
Jiselle Liu <jiselle03@gmail.com>
2020-10-24 22:30:13 -0700

    Remove attributes

--- server/app/serializers/user_serializer.rb
+++ server/app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :company, :email, :phone, :role, :created_at, :updated_at
+  attributes :id, :first_name, :last_name, :company, :email, :phone
 
   # has_many :favorites
 end
