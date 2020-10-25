3f83a1548c7f92e3a50e5d4ce8288abd05c65367
Jiselle Liu <jiselle03@gmail.com>
2020-10-25 13:55:35 -0700

    Change layout to accordion style

--- client/src/components/pages/AccountShowPage.js
+++ client/src/components/pages/AccountShowPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { Box, Paper, Typography,
+import { Accordion, AccordionDetails, AccordionSummary, 
+         Box, Icon, Paper, Typography,
          Table, TableBody, TableCell, TableContainer, TableRow
 } from '@material-ui/core';
 
@@ -8,6 +10,7 @@ import { globalStyles } from '../Styles';
 
 const AccountShowPage = props => {
     const { first_name, last_name, company, email, phone } = props.currentUser;
+    const { favorites } = props;
 
     const global = globalStyles();
 
@@ -32,31 +35,57 @@ const AccountShowPage = props => {
 
     return(
         <>
-            <Typography variant="h6">Profile</Typography>
+            <Typography variant="h4">Hello, {first_name}!</Typography>
 
-            <TableContainer component={Paper}>
-                <Table>
-                    <TableBody>
-                    {profile.map(row => (
-                        <TableRow key={row.title}>
-                            <TableCell>
-                                <Box fontWeight={700}>{row.title}: </Box> 
-                            </TableCell>
-                            <TableCell>
-                                {row.info}
-                            </TableCell>
-                        </TableRow>    
-                    ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<Icon className="fas fa-chevron-down" />}
+                    aria-controls="profile-content"
+                    id="profile-header"
+                >
+                    <Typography variant="h6">Account Details & Security</Typography>
+                </AccordionSummary>
+                <AccordionDetails className={global.flexColumn}>
+                    <TableContainer component={Paper}>
+                        <Table>
+                            <TableBody>
+                            {profile.map(row => (
+                                <TableRow key={row.title}>
+                                    <TableCell>
+                                        <Box fontWeight={700}>{row.title}: </Box> 
+                                    </TableCell>
+                                    <TableCell>
+                                        {row.info}
+                                    </TableCell>
+                                </TableRow>    
+                            ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
 
-            <Typography variant="h6">Security</Typography>
+                    <Typography paragraph>Edit your profile or change your password here.</Typography>
+                </AccordionDetails>
+            </Accordion>
 
-            <Paper className={global.paper}><Typography paragraph>Change your password.</Typography></Paper>
-
-            <Typography variant="h6">Favorites</Typography>
-            <Paper className={global.paper}>&nbsp;</Paper>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<Icon className="fas fa-chevron-down" />}
+                    aria-controls="profile-content"
+                    id="profile-header"
+                >
+                    <Typography variant="h6">Favorites</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    {!favorites && (
+                        <Typography paragraph>You do not have any favorites yet. <Link className={global.link} to='/catalog'>Browse our catalog</Link> to get started.</Typography>
+                    )}
+                    {favorites && (
+                        favorites.map(favorite => (
+                            <Typography>{favorite.name}</Typography>
+                        ))
+                    )}
+                </AccordionDetails>
+            </Accordion>
         </>
     );
 };
