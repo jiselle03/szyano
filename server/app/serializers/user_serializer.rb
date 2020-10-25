9fbe5bb1de2236d6f09694072942f1a002b35f44
Jiselle Liu <jiselle03@gmail.com>
2020-10-24 20:47:14 -0700

    Add attributes

--- server/app/serializers/user_serializer.rb
+++ server/app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :phone, :created_at, :updated_at
+  attributes :id, :first_name, :last_name, :company, :email, :phone, :role, :created_at, :updated_at
 
   # has_many :favorites
 end
