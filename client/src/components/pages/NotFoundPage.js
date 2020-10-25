8f7264aba2b8f313a84a43c797d70ee4fb187a7d
Jiselle Liu <jiselle03@gmail.com>
2020-10-24 22:28:42 -0700

    Create NotFoundPage

---
+++ client/src/components/pages/NotFoundPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Typography } from '@material-ui/core';
+
+const NotFoundPage = () => {
+    return (
+        <Typography variant="h2" style={{color: "maroon"}}>
+            404 Not Found
+        </Typography>
+    );
+};
+
+export default NotFoundPage;
