9af7d29c7543c050f2f0ea2b61aa841779e67bde
Jiselle Liu <jiselle03@gmail.com>
2020-10-24 20:45:51 -0700

    Disable api_only config to include session middleware

--- server/config/application.rb
+++ server/config/application.rb
@@ -32,7 +32,7 @@ module Szyano
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.api_only = false
 
     # Never generate helper and asset files when controllers are created by default
     config.generators do |g|
