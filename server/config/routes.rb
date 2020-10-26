a078fba508f90d126343dcb179d4a61b6b81cbf8
Jiselle Liu <jiselle03@gmail.com>
2020-10-25 17:48:05 -0700

    Add routes for products

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
         get :current, on: :collection
       end
       resource :session, only: [:create, :destroy]
+
+      resources :products
     end
   end
 end
