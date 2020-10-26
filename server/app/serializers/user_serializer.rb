c28925dc286c30b2067c316453edb38c87f4a21b
Jiselle Liu <jiselle03@gmail.com>
2020-10-26 13:16:57 -0700

    Show product info on user favorites

--- server/app/serializers/user_serializer.rb
+++ server/app/serializers/user_serializer.rb
@@ -1,5 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :company, :email, :phone
 
-  # has_many :favorites
+  has_many :favorites
+
+  class FavoriteSerializer < ActiveModel::Serializer
+    attributes :product
+  end
 end
