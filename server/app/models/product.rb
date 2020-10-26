395cb14fb7291a57f3e22c32ed57231e83d4f583
Jiselle Liu <jiselle03@gmail.com>
2020-10-25 21:27:33 -0700

    Add relationships and validations

--- server/app/models/product.rb
+++ server/app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ApplicationRecord
+    has_many :favorites, dependent: :destroy
+    has_many :favorite_collectors, through: :favorites, source: :user
+
+    validates :title, presence: true, uniqueness: true
+    validates :category, presence: true
+    validates :description, length: { minimum: 5, maximum: 750 }
+
+    before_save :capitalize_title
+
+    private
+
+    def capitalize_title
+        self.title.capitalize!
+    end
 end
