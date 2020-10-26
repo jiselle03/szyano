7ff13141d746cfa9c244859af6ffb660a2eaa34b
Jiselle Liu <jiselle03@gmail.com>
2020-10-25 21:27:52 -0700

    Add relationships

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-    # has_many :favorites, dependent: :destroy
+    has_many :favorites, dependent: :destroy
+    has_many :favorited_products, through: :favorites, source: :product
     
     validates :email, presence: true, uniqueness: true,
     format: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
