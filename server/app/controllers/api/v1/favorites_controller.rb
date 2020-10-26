ce752c7dba785cdc17271ba6c60a8779bba5e6f7
Jiselle Liu <jiselle03@gmail.com>
2020-10-26 13:17:13 -0700

    Add renders for create and destroy

--- server/app/controllers/api/v1/favorites_controller.rb
+++ server/app/controllers/api/v1/favorites_controller.rb
@@ -5,12 +5,31 @@ class Api::V1::FavoritesController < ApplicationController
 
     def create
         favorite = Favorite.new(user: current_user, product: @product)
+
+        if favorite.save
+            render json: { 
+                id: favorite.id 
+            }
+        else
+            render(
+                json: { 
+                    errors: favorite.errors 
+                },
+                status: 422 #Unprocessable Entity
+            )
+        end
     end
 
     def destroy
         favorite = current_user.favorites.find params[:id]
         if can? :destroy, favorite
             favorite.destroy
+            render(
+                json: { 
+                    status: 200 
+                }, 
+                status: 200
+            ) 
         end
     end
 
