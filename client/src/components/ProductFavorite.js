f35e57f63c8dfab28aff7ae83312312a1e7e8e4a
Jiselle Liu <jiselle03@gmail.com>
2020-10-26 22:35:26 -0700

    Move favorite function to parent component and fix function to match json output

--- client/src/components/ProductFavorite.js
+++ client/src/components/ProductFavorite.js
@@ -1,30 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 
 import { Icon, IconButton } from '@material-ui/core';
 
-import Favorite from '../api/favorite';
-
-import { cardStyles, globalStyles } from './Styles';
+import { cardStyles } from './Styles';
 
 const ProductFavorite = props => {
-    const { currentUser, favorites, productID } = props;
+    const { favoriteProduct, favorites, productID } = props;
+
     const [isFaved, setIsFaved] = useState(false);
 
     const card = cardStyles();
-    const global = globalStyles();
 
     const handleFavorite = event => {
-        event.preventDefault();
-        const product = parseInt(event.target.parentNode.parentNode.dataset.product);
-        const favorite = currentUser.favorites.filter(fave => fave.product_id === product)[0];
-
-        const params = {
-            product_id: productID,
-            user_id: currentUser.id
-        };
-
-        isFaved ? Favorite.destroy(favorite.id) : Favorite.create(productID, params);
+        favoriteProduct(event);
         setIsFaved(!isFaved);
     };
 
@@ -38,7 +26,7 @@ const ProductFavorite = props => {
             aria-label='add to favorites' 
             onClick={event => handleFavorite(event)}
             className={isFaved ? card.liked : card.unliked}
-            data-product={productID}
+            data-id={productID}
         >
             <Icon className='fas fa-heart' />
         </IconButton>
--- client/src/components/pages/ProductIndexPage.js
+++ client/src/components/pages/ProductIndexPage.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { Button, Card, CardActions, CardContent, Typography } from '@material-ui/core';
 
 import Product from '../../api/product';
+import Favorite from '../../api/favorite';
 
 import ProductFavorite from '../ProductFavorite';
 
@@ -11,20 +12,41 @@ import { cardStyles, globalStyles } from '../Styles';
 
 const ProductIndexPage = props => {
     const { currentUser } = props;
+
     const [products, setProducts] = useState([]);
     const [favorites, setFavorites] = useState([]);
 
+    const ids = currentUser.favorites.map(fave => fave.product.id);
+
     const card = cardStyles();
     const global = globalStyles();
+
+    const favoriteProduct = event => {
+        event.preventDefault();
+        const id = parseInt(event.target.parentNode.parentNode.dataset.id);
+        const favorite = currentUser.favorites.filter(fave => fave.product.id === id)[0];
+
+        const params = {
+            product_id: id,
+            user_id: currentUser.id
+        };
+
+        if (favorite) {
+            Favorite.destroy(favorite.id);
+            setFavorites([...favorites, parseInt(id)]);
+        } else {
+            Favorite.create(id, params);
+            const filteredFaves = favorites.filter(productId => productId !== parseInt(id));
+            setFavorites(filteredFaves);
+        };
+    };
 
     useEffect(() => {
         Product.all().then(products => setProducts(products));
     }, []);
 
     useEffect(() => {
-        if (currentUser) {
-            currentUser.favorites.map(fave => setFavorites([...favorites, fave.product_id]));
-        };
+        if (currentUser) setFavorites(ids);
     }, [currentUser]);
 
     return(
@@ -42,7 +64,7 @@ const ProductIndexPage = props => {
                             <Button>View Product</Button>
                         </Link>
                         
-                        <ProductFavorite currentUser={currentUser} favorites={favorites} productID={product.id} />
+                        <ProductFavorite favoriteProduct={favoriteProduct} favorites={favorites} productID={product.id} />
                     </CardActions>
                 </Card>
                 
