341eadbaf1c371c489aa793cdaabd00d0b720430
Jiselle Liu <jiselle03@gmail.com>
2020-10-28 11:16:48 -0700

    Add Post model and add relationships and validations

---
+++ server/app/models/post.rb
@@ -0,0 +1,21 @@
+class Post < ApplicationRecord
+  belongs_to :user
+
+  # has_many :comments, dependent: destroy
+  # has_many :likes, dependent: :destroy
+  # has_many :likers, through: :likes, source: :user
+  # has_many :taggings, dependent: :destroy
+  # has_many :tags, through: :taggings
+
+  validates :title, presence: true, uniqueness: true
+  validates :category, presence: true, 
+  validates :body, length: { minimum: 100 }
+
+  before_save :capitalize_title
+
+  private
+
+  def capitalize_title
+      self.title.capitalize!
+  end
+end
