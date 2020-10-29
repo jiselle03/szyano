f2e1227808c3fe7145b594210a3a204d790547c6
Jiselle Liu <jiselle03@gmail.com>
2020-10-28 20:56:22 -0700

    Create About page and get info from profile

---
+++ client/src/components/pages/AboutPage.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+
+import { Typography } from '@material-ui/core';
+
+import Profile from '../../api/profile';
+
+const AboutPage = props => {
+    const [profile, setProfile] = useState([]);
+
+    useEffect(() => {
+        Profile.get().then(profile => {
+            setProfile(profile.about.split("//"));
+        });
+    }, []);
+
+    return(
+        <>
+            <Typography variant='h5'>About Us</Typography>
+
+            {profile.map((paragraph, i) => (
+                <Typography paragraph key={i}>{paragraph}</Typography>
+            ))}
+        </>
+    );
+};
+
+export default AboutPage;
