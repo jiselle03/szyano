0c546136b8a5baf1dd8a3dd098ba7b9f2db423fe
Jiselle Liu <jiselle03@gmail.com>
2020-11-05 16:49:26 -0800

    Add createProduct function

--- client/src/components/pages/ProductNewPage.js
+++ client/src/components/pages/ProductNewPage.js
@@ -1,19 +1,59 @@
 import React from 'react';
 
-import { Button } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
+
+import Product from '../../api/product';
+
+import FormField from '../FormField';
+
+import { formStyles, globalStyles } from '../Styles';
 
 const ProductNewPage = props => {
+    const [errors, setErrors] = useState([]);
+
+    const form = formStyles();
+    const global = globalStyles();
+
+    const createProduct = event => {
+        event.preventDefault();
+        const { currentTarget: form } = event;
+        const fd = new FormData(form);
+        setErrors([]);
+        
+        Product.create({
+            title: fd.get("title"),
+            category: fd.get("category"),
+            description: fd.get("description"),
+            model_number: fd.get("model_number"),
+            image1: fd.get("image1"),
+            image2: fd.get("image2"),
+            image3: fd.get("image3")
+        }).then(data => {
+            if (!data.errors) {
+                props.history.push(`/products/${data.id}`);
+            } else {
+                setErrors(data.errors);
+            };
+        });
+
+        form.reset();
+    };
+
     return(
-        <Button
-            variant="contained"
-            component="label"
-        >
-        Upload File
-        <input
-            type="file"
-            style={{ display: "none" }}
-        />
-        </Button>
+        <>
+            <form onSubmit={createProduct} className={global.flexColumn}>
+                <Button
+                    variant="contained"
+                    component="label"
+                >
+                Upload File
+                <input
+                    type="file"
+                    style={{ display: "none" }}
+                />
+                </Button>
+            </form>
+        </>
     );
 };
 
