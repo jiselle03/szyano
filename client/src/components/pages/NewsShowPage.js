b28a570838649e7869e8291a678a5baabdda2132
Jiselle Liu <jiselle03@gmail.com>
2020-11-18 23:48:40 -0800

    Add edit button

--- client/src/components/pages/NewsShowPage.js
+++ client/src/components/pages/NewsShowPage.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
-import { Typography } from '@material-ui/core';
+import { Fab, Icon, Typography } from '@material-ui/core';
 
 import Post from '../../api/post';
 
+import { globalStyles } from '../Styles';
+
 const NewsShowPage = props => {
     const [post, setPost] = useState({});
+
+    const global = globalStyles();
     
     useEffect(() => {
         Post.one(props.match.params.id).then(post => { 
@@ -19,6 +23,10 @@ const NewsShowPage = props => {
 
             <Typography paragraph>{post.category}</Typography>
             <Typography paragraph>{post.body}</Typography>
+
+            <Fab color="secondary" aria-label="edit" className={global.fab}>
+                <Icon className="fas fa-pen" />
+            </Fab>
         </>
     );
 };
